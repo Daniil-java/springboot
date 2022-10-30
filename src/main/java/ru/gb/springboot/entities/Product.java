ef88d68e4e84340205feb929cf532d1f0b7babaf
ПК <saponembigas@gmail.com>
2022-10-30 17:06:30 +0300

    feat/lesson8-hw

--- src/main/java/ru/gb/springboot/entities/Product.java
+++ src/main/java/ru/gb/springboot/entities/Product.java
@@ -8,11 +8,20 @@ import javax.persistence.*;
 @Entity(name="products")
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "title")
     private String title;
     @Column(name = "cost")
     private int cost;
 
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
 }
