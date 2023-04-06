ce3083c2014f719d2e05300faad55868335f5df3
ПК <saponembigas@gmail.com>
2023-04-06 19:41:06 +0700

    feat/builder

--- api/src/main/java/ru/gb/api/ProductDto.java
+++ api/src/main/java/ru/gb/api/ProductDto.java
@@ -8,6 +8,22 @@ public class ProductDto {
     private BigDecimal price;
     private String categoryTitle;
 
+    private ProductDto(Builder builder) {
+        setId(builder.id);
+        setTitle(builder.title);
+        setPrice(builder.price);
+        setCategoryTitle(builder.categoryTitle);
+    }
+
+    public static Builder newBuilder(ProductDto copy) {
+        Builder builder = new Builder();
+        builder.id = copy.getId();
+        builder.title = copy.getTitle();
+        builder.price = copy.getPrice();
+        builder.categoryTitle = copy.getCategoryTitle();
+        return builder;
+    }
+
     public Long getId() {
         return id;
     }
@@ -48,5 +64,44 @@ public class ProductDto {
         this.title = title;
         this.price = price;
         this.categoryTitle = categoryTitle;
+    }
+
+
+    public static final class Builder {
+        private Long id;
+        private String title;
+        private BigDecimal price;
+        private String categoryTitle;
+
+        private Builder() {
+        }
+
+        public static Builder newBuilder() {
+            return new Builder();
+        }
+
+        public Builder withId(Long val) {
+            id = val;
+            return this;
+        }
+
+        public Builder withTitle(String val) {
+            title = val;
+            return this;
+        }
+
+        public Builder withPrice(BigDecimal val) {
+            price = val;
+            return this;
+        }
+
+        public Builder withCategoryTitle(String val) {
+            categoryTitle = val;
+            return this;
+        }
+
+        public ProductDto build() {
+            return new ProductDto(this);
+        }
     }
 }
